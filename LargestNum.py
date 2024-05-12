ba58d10394232e17710c7b467eacb99eb6274879
Mariana Castillo <166894052+CastilloMariana@users.noreply.github.com>
2024-05-12 01:00:50 -0500

    Add files via upload
    This is just for practice.

---
+++ LargestNum.py
@@ -0,0 +1,14 @@
+number_1 = int(input("Enter a number: "))
+number_2 = int (input("Enter another number: "))
+number_3= int(input("Enter one more number: "))
+
+largest_number = number_1
+
+if number_2 > largest_number:
+    largest_number = number_2
+    
+if number_3 > largest_number:
+    largest_number = number_3
+    
+    print("The largest number is: ",largest_number)
+
